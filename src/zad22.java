1d39c842385dabeb38e0b283f1ba347d9c76bf0d
Qrza88 <arleta.kurkiewicz@gmail.com>
2021-11-28 21:33:44 +0100

    handle user input instead of a fixed number

--- src/zad22.java
+++ src/zad22.java
@@ -1,9 +1,13 @@
+import java.util.Scanner;
+
 public class zad22 {
     public static void main(String[] args) {
 //        Napisz program, który sprawdzi czy w tablicy intów znajduje się podana liczba, nie korzytaj z gotowych metod.
 
         int[] array = new int[]{1, 3, 5, 7, 10, 15};
-        int x = 5;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj liczbę");
+        int x= scanner.nextInt();
 
         for (int i = 0; i < array.length; i++) {
             if (x == array[i]) {
